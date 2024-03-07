a407e1f02da68e11347a3ed54083226bdc782ed3
Jose Tejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-03-07 09:49:47 -0600

    reoranizacion de codigo con respecto a admin y client

--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -1,32 +1,10 @@
 import Role from '../role/role.model.js';
-import Admin from '../admin/admin.model.js';
 import Category from '../category/category.model.js';
 
 export const isRoleValid = async (role = '') => {
     const existeRol = await Role.findOne({ role });
     if (!existeRol) {
         throw new Error(`Role ${role} does not exist in the database`);
-    }
-}
-
-export const existEmailAdmin = async (email = '') => {
-    const existeEmail = await Admin.findOne({ email });
-    if (existeEmail) {
-        throw new Error(`Email ${email} is already registered`);
-    }
-}
-
-export const existAdminById = async (id = '') => {
-    const existAdmin = await Admin.findOne({ id });
-    if (existAdmin) {
-        throw new Error(`Admin with ${id} does not exist`)
-    }
-}
-
-export const disableAdmin = async (id = '') => {
-    const adminStatus = await Admin.findOne({ id });
-    if (!adminStatus.status) {
-        throw new Error(`Admin with ${id} is alredy disable`)
     }
 }
 
