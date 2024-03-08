141c65d0218ff65df87c4558b20a2d26aeecf47b
Jose Tejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-03-08 06:55:44 -0600

    se realiza login

--- config/server.js
+++ config/server.js
@@ -5,12 +5,16 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { dbConnection } from './mongo.js';
+import authRoutes from '../src/auth/auth.routes.js';
 import userRoutes from '../src/user/user.router.js';
 import categoryRoutes from '../src/category/category.routes.js';
+
+
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.authPath = '/api/v1/auth'
         this.userPath = '/api/v1/users' 
         this.categoryPath = '/api/v1/categories'
 
@@ -32,6 +36,7 @@ class Server {
     };
 
     routes() {
+        this.app.use(this.authPath, authRoutes);
         this.app.use(this.userPath, userRoutes);
         this.app.use(this.categoryPath, categoryRoutes);
     };
